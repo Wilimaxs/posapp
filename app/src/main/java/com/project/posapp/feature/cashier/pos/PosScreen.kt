762583164dca924ddb26109ef84fc0b3d0b9e7e2
wildantisna <wildan27370@gmail.com>
2026-08-16 15:22:51 +0700

    ADD clear search icon to `AppForm` in POS screen; implement trailing icon logic for resetting search query.

--- app/src/main/java/com/project/posapp/feature/cashier/pos/PosScreen.kt
+++ app/src/main/java/com/project/posapp/feature/cashier/pos/PosScreen.kt
@@ -10,6 +10,10 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Clear
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -65,7 +69,23 @@ fun PosScreen(
                 ),
                 keyboardOptions = KeyboardOptions(
                     imeAction = ImeAction.Search
-                )
+                ),
+                trailingIcon = if (state.searchQuery.isNotEmpty()) {
+                    {
+                        IconButton(
+                            onClick = {
+                                viewModel.onSearchChange(query = "")
+                            }
+                        ) {
+                            Icon(
+                                imageVector = Icons.Outlined.Clear,
+                                contentDescription = "Hapus pencarian"
+                            )
+                        }
+                    }
+                } else {
+                    null
+                }
             )
             CategorySelector(
                 categories = state.categories,
